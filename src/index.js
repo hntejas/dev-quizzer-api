7512d3d483bb836a63f0ec681afdac68d4c988cf
Tejas <hntejas@gmail.com>
2021-07-01 13:51:22 +0530

    feat: setting up graphql server for quizes

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const { ApolloServer } = require("apollo-server");
+const fs = require("fs");
+const path = require("path");
+const quizes = require("./data");
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of dev quizzer`,
+    quizes: () => quizes,
+    quiz: (_, { quizId }) => quizes.find((quiz) => quiz.quizId === quizId),
+  },
+  Quiz: {
+    quizId: (parent) => parent.quizId,
+    quizName: (parent) => parent.quizName,
+    quizCardIcon: (parent) => parent.quizCardIcon,
+    totalQuestions: (parent) => parent.questions.length,
+    questions: (parent) => parent.questions,
+    answers: (parent) => parent.answers,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
+  resolvers,
+});
+
+server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
